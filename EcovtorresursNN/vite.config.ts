61e93282ea1737d0ee9c00267e78ef52d5bb07ab
Guitaramust <guitaramust@gmail.com>
2026-04-17 21:24:12 +0300

    Update vite.config.ts

--- EcovtorresursNN/vite.config.ts
+++ EcovtorresursNN/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/EcovtorresursNN/",
+  base: "./", // Теперь пути в билд-файлах будут начинаться с ./ и искать файлы рядом
 });
