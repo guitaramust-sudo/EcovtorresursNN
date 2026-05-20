a7b4fd8f2e13e3d99a95c9d71ecd1ea57a926c1c
Guitaramust <guitaramust@gmail.com>
2026-05-20 17:38:29 +0300

    Update vite.config.ts

--- EcovtorresursNN/vite.config.ts
+++ EcovtorresursNN/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./", // <-- ДОБАВЬТЕ ЭТУ СТРОКУ! Относительные пути для GitHub Pages
   server: {
     host: "0.0.0.0", // Слушаем все интерфейсы (включая VPN)
     port: 5173, // Твой порт
